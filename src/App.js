855eaee3ba1fe451848b0a8fe3ed0737dcccc9bc
surajhell88 <surajhell88@gmail.com>
2018-04-26 11:51:19 +0530

    Star Rating Filter Component

--- src/App.js
+++ src/App.js
@@ -1,70 +1,55 @@
 import React, { Component } from 'react';
 import './App.css';
-import StartSVG from "./components/star-svg";
+import StartRating from "./components/star-rating";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      	<div className="Container">
-      		<div className="Action-Bar">
-      			<div className="Action">
-      				<div className="Action-Input">
-      					<input type="text" className="Input Input-Text" placeholder="Search" />
-      				</div>
-  				</div>
-      			<div className="Action">
-      				<div className="Action-Input Action-Select Mr-10">
-		      			<select name="group-by" className="Input Input-Select">
-		      				<option value="">Group By</option>
-		      				<option value="day">Day</option>
-		      				<option value="week">Week</option>
-		      				<option value="month">Month</option>
-		      			</select>
-      				</div>
-      				<div className="Action-Input Action-Select">
-		      			<select name="order-by" className="Input Input-Select">
-		      				<option value="">Order By</option>
-		      				<option value="day">Asc</option>
-		      				<option value="week">Desc</option>
-		      			</select>
-      				</div>
-      			</div>
-      			<div className="Filter">
-      				<h4>Filter By:</h4>
-      				<div className="Star-Filter">
-      					<div className="Star-Container">
-      						<input type="checkbox" className="Input" />
-  							<h4 className="Star-Number">1</h4>
-							<StartSVG />
-      					</div>
-      					<div className="Star-Container">
-      						<input type="checkbox" className="Input" />
-  							<h4 className="Star-Number">2</h4>
-      						<StartSVG />
-      					</div>
-      					<div className="Star-Container">
-      						<input type="checkbox" className="Input" />
-  							<h4 className="Star-Number">3</h4>
-      						<StartSVG />
-      					</div>
-      					<div className="Star-Container">
-      						<input type="checkbox" className="Input" />
-  							<h4 className="Star-Number">4</h4>
-      						<StartSVG />
-      					</div>
-      					<div className="Star-Container">
-      						<input type="checkbox" className="Input" />
-  							<h4 className="Star-Number">5</h4>
-      						<StartSVG />
-      					</div>
-      				</div>
-      			</div>
-      		</div>
-      	</div>
-      </div>
-    );
-  }
+	state = {
+		rating: 0,
+	}
+	onRateChange = (rating) => {
+		this.setState({
+			rating,
+		});
+	}
+	render() {
+		return (
+			<div className="App">
+				<div className="Container">
+					<div className="Action-Bar">
+						<div className="Action">
+							<div className="Action-Input">
+								<input type="text" className="Input Input-Text" placeholder="Search" />
+							</div>
+							</div>
+						<div className="Action">
+							<div className="Action-Input Action-Select Mr-10">
+								<select name="group-by" className="Input Input-Select">
+									<option value="">Group By</option>
+									<option value="day">Day</option>
+									<option value="week">Week</option>
+									<option value="month">Month</option>
+								</select>
+							</div>
+							<div className="Action-Input Action-Select">
+								<select name="order-by" className="Input Input-Select">
+									<option value="">Order By</option>
+									<option value="day">Asc</option>
+									<option value="week">Desc</option>
+								</select>
+							</div>
+						</div>
+						<div className="Filter">
+							<h4>Filter By:</h4>
+							<StartRating
+								rating={this.state.rating}
+								onRateChange={this.onRateChange}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/star-rating.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import StartSVG from "./star-svg";
+
+const StarRating = (props) => {
+  	const { rating, onRateChange } = props;
+  	return (
+		<div className="Star-Filter">
+			<div className="Star-Container">
+				<label className="Star-Label" htmlFor="star-rating-1">
+					<input 
+						type="checkbox" 
+						className="Input" 
+						id="star-rating-1"
+						checked={rating >= 1} 
+						onChange={() => onRateChange(1)} 
+					/>
+					<h4 className="Star-Number">1</h4>
+					<StartSVG />
+				</label>
+			</div>
+			<div className="Star-Container">
+				<label className="Star-Label" htmlFor="star-rating-2">
+					<input 
+						type="checkbox" 
+						className="Input" 
+						id="star-rating-2"
+						checked={rating >= 2} 
+						onChange={() => onRateChange(2)} 
+					/>
+					<h4 className="Star-Number">2</h4>
+					<StartSVG />
+				</label>
+			</div>
+			<div className="Star-Container">
+				<label className="Star-Label" htmlFor="star-rating-3">
+					<input 
+						type="checkbox" 
+						className="Input" 
+						id="star-rating-3"
+						checked={rating >= 3} 
+						onChange={() => onRateChange(3)} 
+					/>
+					<h4 className="Star-Number">3</h4>
+					<StartSVG />
+				</label>
+			</div>
+			<div className="Star-Container">
+				<label className="Star-Label" htmlFor="star-rating-4">
+					<input 
+						type="checkbox" 
+						className="Input" 
+						id="star-rating-4"
+						checked={rating >= 4} 
+						onChange={() => onRateChange(4)} 
+					/>
+					<h4 className="Star-Number">4</h4>
+					<StartSVG />
+				</label>
+			</div>
+			<div className="Star-Container">
+				<label className="Star-Label" htmlFor="star-rating-5">
+					<input 
+						type="checkbox" 
+						className="Input" 
+						id="star-rating-5"
+						checked={rating >= 5} 
+						onChange={() => onRateChange(5)} 
+					/>
+					<h4 className="Star-Number">5</h4>
+					<StartSVG />
+				</label>
+			</div>
+		</div>
+  	);
+};
+
+export default StarRating;
